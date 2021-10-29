889977585cdf17675cea710c7e7e8f5411ff194b
cc710788@gmail.com <cc710788@gmail.com>
2021-10-29 12:30:35 +0530

    last

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const loginCall=async(userCredentials,dispatch)=>{
     dispatch({type:"LOGIN_START"})
     try{
-    const res = await axios.post(process.env.REACT_APP_URL+'authi/login',userCredentials)
+    const res = await axios.post(process.env.REACT_APP_URL+'auth/login',userCredentials)
     dispatch({type:"LOGIN_SUCCESS",payload:res.data.user})
     }catch(err){
         dispatch({type:"LOGIN_FAILURE",payload:err})
