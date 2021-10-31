41efa94b3c36739a867bbe676eecc20fb7c4217d
cc710788@gmail.com <cc710788@gmail.com>
2021-10-31 11:23:21 +0530

    lastt

--- src/serverProxy.js
+++ src/serverProxy.js
@@ -1,15 +1,16 @@
 const {createPxoxyMiddleware} = require('http-proxy-middleware')
 module.exports=app=>{
+     app.use(
+        createPxoxyMiddleware('post/timeline/:id',{
+            target:'https://fb-clone-backend-1.herokuapp.com/api/',
+            changeOrigin:true
+        })
+    )
     app.use(
         createPxoxyMiddleware('user/friends/:id',{
             target:'https://fb-clone-backend-1.herokuapp.com/api/',
             changeOrigin:true
         })
     )
-    app.use(
-        createPxoxyMiddleware('post/timeline/:id',{
-            target:'https://fb-clone-backend-1.herokuapp.com/api/',
-            changeOrigin:true
-        })
-    )
+   
 }
